d478924d6bbeb5c3b333268c55cf3183368d2542
BAMBA WINGNEMILA EDWIGE <bambawingnemilaedwige@gmail.com>
2026-02-02 00:51:02 +0000

    mon portfolio version finale

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
